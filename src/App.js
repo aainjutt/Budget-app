355003d040b85c5780a59989678013eeb733b35f
Aain Ur Rehman <aain134@gmail.com>
2023-12-04 15:44:45 +0500

    Add Form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
   return (
    <div>
     <Header/>
+    <Form/>
    </div>
   );
 }
